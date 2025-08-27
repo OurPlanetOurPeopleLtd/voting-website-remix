ac0f5324a795cdbfbc8b839366ba6a0f0021ae16
Chris Pepper <chrispepper1989@gmail.com>
2025-08-27 02:41:50 +0100

    fix: all routes now loading a suitable page

--- app/components/old_pages/LoadingPage.js
+++
@@ -1,6 +0,0 @@
-﻿const LoadingPage = () => {
-    return <div className="spinner-border text-primary" role="status">
-        <span className="visually-hidden">Loading...</span>
-    </div>;
-};
-export default LoadingPage;
